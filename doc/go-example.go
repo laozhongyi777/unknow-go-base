a9439e315d41a557d3359a0ad56174ee3267d33c
laozhongyi777 <qjkk88369yy@gmail.com>
2019-03-02 23:30:28 +0400

    学习打开第一天　2019-3-2

---
+++ doc/go-example.go
@@ -0,0 +1,33 @@
+//当前程序包名
+package doc
+
+//导入其他报的包
+import "fmt"
+
+//常量定义
+
+const PI = 3.1425926
+
+//声明全局变量与赋值
+
+var name = "gopher"
+
+//一般类型声明
+
+type Newtpye int
+
+//结构的声明
+
+type gopher struct {
+}
+
+//接口的声明
+
+type golang interface {
+}
+
+//由main函数作为程序的入口
+
+func main() {
+	fmt.Printf("hello, world, 你好，世界")
+}
