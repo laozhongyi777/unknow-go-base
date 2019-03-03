3d548d9dc68d57d261e73db8443d3c824cf47dfc
laozhongyi777 <qjkk88369yy@gmail.com>
2019-03-04 00:39:17 +0400

    学习打开第２天　2019-3-３　　变量　　学习是个持续过程，但是不能停

--- src/study/var-test.go
+++ src/study/var-test.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var a int = 65 //string　　类型是　６５个字节即大写的A
-	b := string(a)
+	//b := string(a)
+	b := strconv.Itoa(a)
 	fmt.Println(b)
 
 }
