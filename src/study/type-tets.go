c3204054a0720e69a7bba1ff937255630ede931b
laozhongyi777 <qjkk88369yy@gmail.com>
2019-03-04 00:35:47 +0400

    学习打开第２天　2019-3-３　　变量　　学习是个持续过程，但是不能停

---
+++ src/study/type-tets.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	fmt.Println(a)
+}
+
+//
+//
+//
---
+++ src/study/var-test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 65 //string　　类型是　６５个字节即大写的A
+	b := string(a)
+	fmt.Println(b)
+
+}
